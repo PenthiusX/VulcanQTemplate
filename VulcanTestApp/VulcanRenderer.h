b72d9bd6fe1bb0cdf1f11360101ff1f781b12089
Aditya Mattoo <aditya.mattoo88@gmail.com>
2020-05-18 14:20:04 +0530

    last commit off the labmachine , need to move this to the local qual machine

---
+++ VulcanTestApp/VulcanRenderer.h
@@ -0,0 +1,122 @@
+#pragma once
+
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+#include <set>
+
+struct QueueFamilyIndices {
+
+	uint32_t graphicsFamily;
+	uint32_t presentFamily;
+
+	bool isComplete() {
+		if (graphicsFamily >= 0 && presentFamily >= 0) {
+			return true;
+		}
+		return false;
+	}
+};
+
+struct SwapChainSupportDetails {
+	VkSurfaceCapabilitiesKHR capabilities;
+	std::vector<VkSurfaceFormatKHR> formats;
+	std::vector<VkPresentModeKHR> presentModes;
+};
+
+
+class VulcanRenderer
+{
+public:
+	VulcanRenderer();
+	~VulcanRenderer();
+
+	void run();
+
+
+
+private:
+
+	//Variables
+	GLFWwindow* window;
+
+	VkInstance instance;
+	VkDebugUtilsMessengerEXT debugMessenger;
+	VkSurfaceKHR surface;
+
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	VkDevice device;
+
+	VkQueue graphicsQueue;
+	VkQueue presentQueue;
+
+	VkSwapchainKHR swapChain;
+	std::vector<VkImage> swapChainImages;
+	VkFormat swapChainImageFormat;
+	VkExtent2D swapChainExtent;
+	std::vector<VkImageView> swapChainImageViews;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
+
+	VkRenderPass renderPass;
+	VkPipelineLayout pipelineLayout;
+	VkPipeline graphicsPipeline;
+
+	VkCommandPool commandPool;
+	std::vector<VkCommandBuffer> commandBuffers;
+
+	std::vector<VkSemaphore> imageAvailableSemaphores;
+	std::vector<VkSemaphore> renderFinishedSemaphores;
+	std::vector<VkFence> inFlightFences;
+	std::vector<VkFence> imagesInFlight;
+	size_t currentFrame = 0;
+
+	const int WIDTH = 800;
+	const int HEIGHT = 600;
+	const int MAX_FRAMES_IN_FLIGHT = 2;
+
+	//Run states
+	void initWindow();
+	void initVulkan();
+	void mainLoop();
+	void cleanup();
+
+	//Vulcan Boiler Plate
+	void createInstance();
+	void createSurface();
+	void pickPhysicalDevice();
+	void createLogicalDevice();
+	void createSwapChain();
+	void createImageViews();
+	void createRenderPass();
+	void createGraphicsPipeline();
+	void createFramebuffers();
+	void createCommandPool();
+	void createCommandBuffers();
+	void createSyncObjects();
+
+	void drawFrame();
+
+	//Helpers
+	VkShaderModule createShaderModule(const std::vector<char>& code);
+	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+	VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes);
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR & capabilities);
+	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+	bool isDeviceSuitable(VkPhysicalDevice device);
+	bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+	std::vector<const char*> getRequiredExtensions();
+	std::vector<char> readFile(const std::string & filename);
+
+};
+
--- VulcanTestApp/shaders/shaderCompile.bat
+++ VulcanTestApp/shaders/shaderCompile.bat
@@ -1,3 +1,6 @@
-C:/VulkanSDK/1.2.131.2/Bin32/glslc.exe 09_shader_base.vert -o vert.spv
-C:/VulkanSDK/1.2.131.2/Bin32/glslc.exe 09_shader_base.frag -o frag.spv
+@echo off
+REM C:/VulkanSDK/1.2.131.2/Bin32/glslc.exe 09_shader_base.vert -o vert.spv
+REM C:/VulkanSDK/1.2.131.2/Bin32/glslc.exe 09_shader_base.frag -o frag.spv
+C:/VulkanSDK/1.2.131.2/Bin32/glslc.exe 17_shader_base.vert -o vert.spv
+C:/VulkanSDK/1.2.131.2/Bin32/glslc.exe 17_shader_base.frag -o frag.spv
 pause
